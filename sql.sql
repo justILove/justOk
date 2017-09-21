7751798d5a422deb24d30db385faeae3b0895df8
justILove <31735144+justILove@users.noreply.github.com>
2017-09-21 12:42:42 +0300

    о

--- sql.sql
+++ sql.sql
@@ -1,3 +1,16 @@
+SELECT 
+proxy.busy as proxy_busy,
+bots.* 
+FROM `bots` 
+LEFT JOIN proxy_bots as pb
+  ON pb.bot = bots.id
+LEFT JOIN proxy 
+  ON pb.proxy = proxy.id
+WHERE
+  pb.proxy = 2
+AND bots.type != "lost_access"
+
+
 SELECT
 	(
 		SELECT count(id) FROM group_ta_gone as gtg WHERE gtg.bot = bots.id
